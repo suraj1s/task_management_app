42693b8c8025826fffe0c0efb9efc6cabf4a0a92
suraj subedi <surajsubedi059@gmail.com>
2024-03-23 23:49:42 +0545

    minor adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,27 @@
 import Link from "next/link";
 import { getServerAuthSession } from "~/server/auth";
-import TaskList from "../components/task/TaskList";
 import CategoryList from "~/components/category/CategoryList";
+
 export default async function Home() {
   const session = await getServerAuthSession();
-
   return (
     <main className="">
       <div className=" flex gap-12 px-4 py-16 ">
         <div className="flex flex-col items-center gap-2">
-          <div className="flex gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
+          <div className="flex items-center  gap-4">
+            <p className="text-center text-xl text-white">
+              {session && (
+                <p>
+                  Logged in as
+                  <span className="font-semibold text-blue-400 pl-2 capitalize text-xl">
+                    {session.user?.name}
+                  </span>
+                </p>
+              )}
             </p>
             <Link
               href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+              className="rounded-full bg-white/10 px-4 py-2 text-sm font-semibold no-underline transition hover:bg-white/20"
             >
               {session ? "Sign out" : "Sign in"}
             </Link>
--- src/server/api/routers/category.ts
+++ src/server/api/routers/category.ts
@@ -20,51 +20,42 @@ export const categoryRouter = createTRPCRouter({
       return createdCategory;
     }),
 
-  // updateOne: protectedProcedure
-  //   .input(
-  //     z.object({
-  //       id: z.number(),
-  //       completed: z.boolean(),
-  //     }),
-  //   )
-  //   .mutation(async ({ ctx, input }) => {
-  //     const id = input.id;
-  //     const updatedCategory = await ctx.db.category.update({
-  //       where: {
-  //         id: input.id,
-  //       },
-  //       data: {
-  //         completed: input.completed,
-  //       },
-  //     });
-  //     return ctx.db.Category.findMany({
-  //       where: {
-  //         createdById: ctx.session.user.id
-  //         },
-  //       })
-  //   }),
-
-  // deleteOne: protectedProcedure
-  //   .input(z.number())
-  //   .mutation(async ({ ctx, input }) => {
-  //     const deletedCategory = await ctx.db.Category.delete({
-  //       where: {
-  //         id: input,
-  //       },
-  //     });
-  //     return ctx.db.cate.findMany({
-  //       where : {
-  //         createdById: ctx.session.user.id
-  //       },
-  //       }); ;
-  //   }),
-  
-  getAllCategorys: protectedProcedure.query( async ({ ctx }) => {
-    const createdCategory = await ctx.db.category.findMany({
-      where : {
-        createdById: ctx.session.user.id
-      },
+  updateOne: protectedProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        name: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const updatedCategory = await ctx.db.category.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+        },
       });
-      return createdCategory;
+      return updatedCategory;
+    }),
+
+  deleteOne: protectedProcedure
+    .input(z.number())
+    .mutation(async ({ ctx, input }) => {
+      const deletedCategory = await ctx.db.category.delete({
+        where: {
+          id: input,
+        },
+      });
+      return deletedCategory;
+    }),
+
+  getAllCategorys: protectedProcedure.query(async ({ ctx }) => {
+    const createdCategory = await ctx.db.category.findMany({
+      where: {
+        createdById: ctx.session.user.id,
+      },
+    });
+    return createdCategory;
   }),
 });
